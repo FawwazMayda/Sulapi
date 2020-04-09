55ca9d4ed5b49abc33f7647ff2e8e48d4a7e6705
Erlina <erlinafabiola09@gmail.com>
2020-04-09 17:32:53 +0700

    Sound Menu (Table View + Segmented Control) + Detail Sound Menu (AVAudioPlayer)

--- Alarmy/ViewController/Sound/SecondSoundVC.swift
+++ Alarmy/ViewController/Sound/SecondSoundVC.swift
@@ -7,24 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SecondSoundVC: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    
+    var titleTemp = ""
+    var durationTemp = ""
+    var imageTemp = ""
+    
+    var player: AVAudioPlayer?
+    var currentTime: TimeInterval = 0
+    
+    @IBAction func didTapButton() {
+        let urlString = Bundle.main.path(forResource: "\(titleTemp)", ofType: "mp3")
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        if let player = player, player.isPlaying {
+            //stop playback
+            button.setImage(UIImage(named: "playBtn"), for: .normal)
+            currentTime = player.currentTime
+            player.pause()
 
-        // Do any additional setup after loading the view.
+        }else {
+            button.setImage(UIImage(named: "pauseBtn"), for: .normal)
+            do {
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                
+                guard let urlString = urlString else {
+                    return
+                }
+                
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                
+                guard let player = player else {
+                    return
+                }
+                
+                if currentTime == 0 {
+                    player.play()
+                } else {
+                    player.currentTime = currentTime
+                    player.play()
+                    //print(currentTime)
+                }
+            } catch  {
+                print("Something went wrong")
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = titleTemp
+        durationLabel.text = durationTemp
+        image.image = UIImage(named: imageTemp)
     }
-    */
-
+    
 }
